525090ebba2cce82a1b8b8b4a32232bd09faf355
IraDei <33973471+IraDei@users.noreply.github.com>
2020-09-11 16:59:32 +0800

    Update img2video.m

--- img2video.m
+++ img2video.m
@@ -1,11 +1,5 @@
 function [aviobj] = img2video(img_seq, nfrm, dir_opt, aviname, ext, frame_rate)
 %IMG2VIDEO 
-% CreatVideoFromPic(dn, picformat,aviname)
-% 将某个文件夹下某种格式的所有图片合成为视频文件
-% dn : 存储图片的文件夹
-% picformat : 要读取的图片的格式，如png、jpg等形式，字符串数组
-% aviname   : 存储的视频的文件名
-% example : CreatVideoFromPic( './', 'png','presentation.avi');
    
     avi_dir = [dir_opt aviname];
     aviobj = VideoWriter([avi_dir ext]);
@@ -16,7 +10,7 @@ function [aviobj] = img2video(img_seq, nfrm, dir_opt, aviname, ext, frame_rate)
             if size(img_seq(:,:,i),1) ~= aviobj.Height || size(img_seq(:,:,i),2) ~= aviobj.Width
                 close(aviobj);
                 delete( aviname )
-                error('所有图片的尺寸要相同！！');
+                error('Inconsistent frame size!');
             end
         end
         
